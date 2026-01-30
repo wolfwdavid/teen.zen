6027d584759c778a5f096131befbe67caddff4ba
David Wolf <cdw8481@nyu.edu>
2026-01-30 17:09:54 -0500

    updated gitignore

--- svelte-teen-zen/vite.config.js
+++ svelte-teen-zen/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/teen.zen/', // Change this to your GitHub repo name
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  base: '/teen.zen/'
 })

ce5395ba24439341f5df2f1cab48e0bb589929ff
Admin <admin@10-20-40-84.dynapool.wireless.nyu.edu>
2026-02-03 14:49:38 -0500

    Update site with correct paths

--- chatbot-rag/frontend/vite.config.mjs
+++ chatbot-rag/frontend/vite.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: '/teen.zen/',
   plugins: [react(), tailwindcss()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   server: {

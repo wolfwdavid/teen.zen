a2da72d512e4b76b5d22b67b716c5c31baab17c1
Admin <admin@10-20-40-84.dynapool.wireless.nyu.edu>
2026-02-03 20:03:14 -0500

    ladning page

--- svelte-teen-zen/src/main.js
+++ svelte-teen-zen/src/main.js
@@ -2,6 +2,12 @@ import './app.css'
 import App from './App.svelte'
 import { mount } from 'svelte'
 
+const app = new App({
+  target: document.getElementById('app'),
+})
+
 mount(App, {
   target: document.getElementById('app'),
 })
+
+export default app
